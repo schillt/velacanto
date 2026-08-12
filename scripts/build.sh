c394bdccfdc3f734c10146ae6a4e97c1250c8f66
Tyler Schill <98628991+schillt@users.noreply.github.com>
2026-08-11 22:58:41 -0400

    Add native player UI test target

--- scripts/build.sh
+++ scripts/build.sh
@@ -105,7 +105,8 @@ lint_swift() {
     --strict \
     --recursive \
     "$project_root/Velacanto" \
-    "$project_root/VelacantoTests"
+    "$project_root/VelacantoTests" \
+    "$project_root/VelacantoUITests"
 }
 
 case "$build_mode" in
